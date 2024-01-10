dad14d9efdb5a9e9eb608bd989298fa37bab971d
huynhhothoty <20110597@student.hcmute.edu.vn>
2024-01-10 15:51:36 +0700

    Update useUser.js

--- client/src/features/account/useUser.js
+++ client/src/features/account/useUser.js
@@ -1,11 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
 import { getCurrentUser } from '../../services/userService';
 
 export function useUser() {
-    const [userToken] = useState(function () {
-        return localStorage.getItem('user_token') ?? '';
-    });
+    const [userToken] = localStorage.getItem('user_token') ?? '';
     const { data: user, isLoading } = useQuery({
         queryKey: ['user', userToken],
         queryFn: () => getCurrentUser(userToken),
