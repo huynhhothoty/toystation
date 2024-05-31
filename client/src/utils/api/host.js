b637d71098767ce29d0e651d9f9d6529fb8eef88
huynhhothoty <20110597@student.hcmute.edu.vn>
2024-05-31 17:24:34 +0700

    update api link

--- client/src/utils/api/host.js
+++ client/src/utils/api/host.js
@@ -1,2 +1,4 @@
-export const host = 'http://localhost:3000/api';
-// export const host = 'https://toystationapi.vercel.app/api';
+export const host =
+    process.env.NODE_ENV === 'production'
+        ? 'https://toystationapi.vercel.app/api'
+        : 'http://localhost:3000/api';
